831387fb6499cca4a7f65cfdd0c9cbc55dfa2b25
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-20 22:00:50 +0200

    complete

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+
+    int result = 1; 
+    for (int i = 0; i < y; i++) {
+        result *= x; 
+    }
+
+    cout << "x^y = " << result << endl;
+    return 0;
+}
