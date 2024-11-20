3f915162bb3eedcf5ade4b267c8fdbe82e4188fc
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-20 22:05:00 +0200

    complete

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int arifmetic = 0;
+	for (int i = 1; i <= 1000; i++) {
+		arifmetic += i;
+	}
+	cout << "Arifmetic sum of numbers from 1 to 1000 is: " << arifmetic << endl;
+
+
+	return 0;
+}
